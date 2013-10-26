3cfd84c2f3b70e71552684aa8387acbfea67a833
Andrew Garner <andrew@andrewgarner.com>
2013-10-26 16:11:29 +0100

    Remove Node.js defaults

--- manifests/site.pp
+++ manifests/site.pp
@@ -63,12 +63,6 @@ node default {
     fail('Please enable full disk encryption and try again')
   }
 
-  # node versions
-  include nodejs::v0_4
-  include nodejs::v0_6
-  include nodejs::v0_8
-  include nodejs::v0_10
-
   # default ruby versions
   include ruby::1_8_7
   include ruby::1_9_2
